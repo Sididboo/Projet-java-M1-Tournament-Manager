7ed042ccf36f93c022a63a2d13de35ad577ce227
Skollid <romainjousseaume24@gmail.com>
2022-11-16 21:47:17 +0100

    apllication.properties to java class config with h2 bdd

---
+++ src/main/java/com/supdevinci/tournamentmanager/constant/EnvConstant.java
@@ -0,0 +1,15 @@
+package com.supdevinci.tournamentmanager.constant;
+
+/**
+ * Define Env variables
+ */
+public class EnvConstant {
+
+    public static final String PRODUCTION = "PRODUCTION";
+    public static final String MYSQL_HOST = "MYSQL_HOST";
+    public static final String MYSQL_PORT = "MYSQL_PORT";
+    public static final String MYSQL_DATABASE = "MYSQL_DATABASE";
+    public static final String MYSQL_USERNAME = "MYSQL_USERNAME";
+    public static final String MYSQL_PASSWORD = "MYSQL_PASSWORD";
+    
+}
