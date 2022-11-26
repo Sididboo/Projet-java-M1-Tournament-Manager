e9832532af65c4926f37d560adf8c25233a7396d
Skollid <romainjousseaume24@gmail.com>
2022-11-26 11:29:32 +0100

    Add data test constant

--- src/test/java/com/supdevinci/tournamentmanager/constant/Constant.java
+++ src/test/java/com/supdevinci/tournamentmanager/constant/Constant.java
@@ -12,6 +12,8 @@ public class Constant {
     // Player
     public static Player P1 = new Player(1L, "P1", "24000");
     public static Player P2 = new Player(2L, "P2", "24001");
+    public static Player P3 = new Player(3L, "P3", "24002");
     // Team
     public static Team T1 = new Team(1L, "T1", Arrays.asList(P1));
+    public static Team T2 = new Team(2L, "T2", Arrays.asList(P2, P3));
 }
